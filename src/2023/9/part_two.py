da7157bdfd232f806104e5de507ed35018727a7c
Mathieu ROUDAUT <m.roudaut56@gmail.com>
2023-12-09 09:28:13 +0100

    feat: add day 9

---
+++ src/2023/9/part_two.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from commons import utils
+
+
+def main(input_lines: list[str]):
+    extrapolated_values = []
+    while len(input_lines) > 0:
+        input_line = [int(measure) for measure in input_lines.pop().strip().split()]
+        sensor_history = [input_line]
+        while sensor_history[-1].count(sensor_history[-1][0]) < len(sensor_history[-1]):
+            sensor_history.append(compute_next_sequence(sensor_history[-1]))
+        current_sequence = sensor_history.pop()
+        while len(sensor_history) > 0:
+            next_sequence = sensor_history.pop()
+            next_sequence = [next_sequence[0] - current_sequence[0]] + next_sequence
+            current_sequence = next_sequence
+        extrapolated_values.append(current_sequence[0])
+    return sum(extrapolated_values)
+
+
+def compute_next_sequence(current_sequence: list[int]):
+    next_sequence = []
+    i = 1
+    while i < len(current_sequence):
+        next_sequence.append(current_sequence[i] - current_sequence[i - 1])
+        i += 1
+    return next_sequence
+
+
+if __name__ == "__main__":
+    print(main(utils.read_file(Path(__file__).parent / "input.txt")))
